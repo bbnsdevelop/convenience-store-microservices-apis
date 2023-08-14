922443c91437973af7018f38e790a40dccefcdc3
bbnsdevelop <bbnsdevelop@gmail.com>
2023-08-14 20:45:45 -0300

    useless import code

--- micro-services/inventory-service/src/main/java/br/com/bbnsdevelop/inventoryservice/services/INventoryService.java
+++ micro-services/inventory-service/src/main/java/br/com/bbnsdevelop/inventoryservice/services/INventoryService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import br.com.bbnsdevelop.inventoryservice.dto.InventoryDto;
 import br.com.bbnsdevelop.inventoryservice.entities.Inventory;
-import br.com.bbnsdevelop.inventoryservice.exceptions.NotFoundException;
 import br.com.bbnsdevelop.inventoryservice.repositories.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
