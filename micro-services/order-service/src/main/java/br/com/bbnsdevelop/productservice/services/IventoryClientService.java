b4a734b66c1059fc6ff1cbb097e735196b49f945
bbnsdevelop <bbnsdevelop@gmail.com>
2023-08-17 22:35:24 -0300

    enable order-service to call mult instances

--- micro-services/order-service/src/main/java/br/com/bbnsdevelop/productservice/services/IventoryClientService.java
+++ micro-services/order-service/src/main/java/br/com/bbnsdevelop/productservice/services/IventoryClientService.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class IventoryClientService {
 
 	
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 	
 	@Value("${server-inventory.url}")
 	private String inventory;
@@ -23,7 +23,7 @@ public class IventoryClientService {
 		String url = inventory.concat("/api/inventories/v1/").concat(code);
 		
 		log.info("call a service inventory to check if has item in stock: {}", url);
-		return webClient.get().uri(url).retrieve().bodyToMono(Boolean.class).block();
+		return webClientBuilder.build().get().uri(url).retrieve().bodyToMono(Boolean.class).block();
 	}
 	
 }
